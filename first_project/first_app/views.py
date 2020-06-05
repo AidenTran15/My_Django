a6ee900332c55334d0aff2ed056092055ad5bad5
AidenTran15 <aidenkiettran@gmail.com>
2020-06-05 15:14:00 +1000

    added ProTwo

--- first_project/first_app/views.py
+++ first_project/first_app/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def index(response):
-    return HttpResponse("Hello World")
+def index(request):
+    my_dict = {'insert_me': "Now I am coming from first app"}
+    return render(request, 'first_app/index.html',context=my_dict)
 
 def apptwo(response):
-    return HttpResponse("<em>My Secon App <em>")
+    return HttpResponse("<em>My Second App <em>")
